93f82a8ba49ee56f92dde8d40e50eab7c3b875b3
yji00 <dksro333@naver.com>
2021-04-07 00:29:20 +0900

    first

---
+++ public/js/restaurant.js
@@ -0,0 +1,10 @@
+$('.modal-wrapper > .modal-wrap i.fa').click(function() {
+	$('.modal-wrapper').css('display', 'none')
+})
+
+$('.container-wrapper > h3.menu').click(function(){
+    $('.modal-wrapper').css('display','block')
+	$('.modal-wrapper .modal-wrap').css('transform', 'scale(0)')
+	$('.modal-wrapper .modal-wrap').css('transform')
+	$('.modal-wrapper .modal-wrap').css('transform', 'scale(1)')
+})
