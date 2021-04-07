018e2450dd399026f2df6a22c653497e9e638d78
yji00 <yji00@naver.com>
2021-04-07 17:55:35 +0900

    0407

--- public/js/restaurant.js
+++ public/js/restaurant.js
@@ -1,10 +1,21 @@
-$('.modal-wrapper > .modal-wrap i.fa').click(function() {
+$('.modal-wrapper > .modal-wrap i.fa').click(function () {
 	$('.modal-wrapper').css('display', 'none')
 })
 
-$('.container-wrapper > h3.menu').click(function(){
-    $('.modal-wrapper').css('display','block')
-	$('.modal-wrapper ul.modal-wrap').css('transform', 'scale(0)')
-	$('.modal-wrapper ul.modal-wrap').css('transform')
-	$('.modal-wrapper ul.modal-wrap').css('transform', 'scale(1)')
-})
+if ($('.con-modal').css('display', 'none')) {
+	$('.container-wrapper > h3.menu').click(function () {
+		$('.menu-modal').css('display', 'block')
+		$('.menu-modal .menu-mo').css('transform', 'scale(0)')
+		$('.menu-modal .menu-mo').css('transform')
+		$('.menu-modal .menu-mo').css('transform', 'scale(1)')
+	})
+}
+
+if ($('.menu-modal').css('display', 'none')) {
+	$('.container-wrapper > h3.contact').click(function () {
+		$('.con-modal').css('display', 'block')
+		$('.con-modal .con-mo').css('transform', 'scale(0)')
+		$('.con-modal .con-mo').css('transform')
+		$('.con-modal .con-mo').css('transform', 'scale(1)')
+	})
+}
